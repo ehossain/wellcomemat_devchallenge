1a0998c7441e2c3e3a335915576854b20a299caa
Elizabeth Hossain <elizabethhossain@gmail.com>
2013-09-24 14:07:48 -0600

    Added more fields from project_management.tasks to Projects controller

--- kohana/application/classes/Controller/Projects.php
+++ kohana/application/classes/Controller/Projects.php
@@ -5,7 +5,10 @@ class Controller_Projects extends Controller {
 	public function action_index(){
 		$tasks = ORM::factory('tasks')->find_all();
 		$projects = ORM::factory('projects')->find_all();
-		$this->response->body('<table cellpadding="2" cellspacing="0" border="1"><tr><th>Project ID</th><th>Project Task</th></tr><tr><td>'.$projects[0]->id.'</td><td>'.$tasks[0]->name.'</td></tr></table>');
+		$this->response->body('<table cellpadding="2" cellspacing="0" border="1">
+		<tr><th>Project ID</th><th>Project Name</th><th>Project Task</th><th>Project Due Date</th><th>Project Completed?</th></tr>
+		<tr><td>'.$tasks[7]->id.'</td><td>'.$projects[7]->name.'</td><td>'.$tasks[7]->name.'</td><td>'.$tasks[7]->due_at.'</td><td>'.$tasks[7]->completed.'</td></tr>
+		</table>');
 		//var_dump($tasks);
 	}
 	
