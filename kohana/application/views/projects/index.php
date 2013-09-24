d422e94ce749df3b2805af539ab782c6ba6ec035
Elizabeth Hossain <elizabethhossain@gmail.com>
2013-09-24 17:29:17 -0600

    Modified projects view to display tasks with tasks.project_ids that match projects.id

--- kohana/application/views/projects/index.php
+++ kohana/application/views/projects/index.php
@@ -1,47 +1,35 @@
-<p>List of Projects</p>
+<h4>List of Projects:</h4>
 
 <table cellpadding="2" cellspacing="0" border="1">
 
-<tr><th>Project ID</th><th>Project Task</th><th>Project Due Date</th><th>Project Completed?</th></tr>
+<tr><th>Project Index ID</th><th>Project ID</th><th>Project Name</th><th>Project Task</th><th>Task Due Date</th><th>Task Completed?</th></tr>
 
 <?php
-foreach ($tasks as $task){
 
+foreach($tasks as $task){
+	
+	foreach($projects as $project){
+		if($project->id == $task->project_id){
+			
 ?>
-<tr>
-
-<td>
-
-<?php echo $task->project_id ?>
-
-</td>
-<td><?php echo $task->name ?></td>
-<td><?php echo $task->due_at ?></td>
-<td><?php echo $task->completed ?></td>
-
-</tr>
+			<tr>
+			
+			<td><?php echo $project->id ?></td>
+			<td><?php echo $task->project_id ?></td>
+			<td><?php echo $project->name ?></td>
+			<td><?php echo $task->name ?></td>
+			<td><?php echo $task->due_at ?></td>
+			<td><?php echo $task->completed ?></td>
+			
+			</tr>
 <?php
+			
+		}
+		
+	}
+	
 }
-?>
 
-</table>
-
-<table cellpadding="2" cellspacing="0" border="1">
-
-<tr><th>Project Index ID</th><th>Project Name</th></tr>
-
-<?php
-foreach ($projects as $project){
-
-?>
-<tr>
-
-<td><?php echo $project->id ?></td>
-<td><?php echo $project->name ?></td>
-
-</tr>
-<?php
-}
 ?>
 
 </table>
