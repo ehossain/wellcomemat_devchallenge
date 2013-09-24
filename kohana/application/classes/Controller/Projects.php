1e66ac629f3850f8a893332123258163e180b4a3
Elizabeth Hossain <elizabethhossain@gmail.com>
2013-09-24 13:58:51 -0600

    Added projects > id from DB to Projects controller and wrapped DB information pulled from item (0) into an HTML table

--- kohana/application/classes/Controller/Projects.php
+++ kohana/application/classes/Controller/Projects.php
@@ -4,7 +4,8 @@ class Controller_Projects extends Controller {
 
 	public function action_index(){
 		$tasks = ORM::factory('tasks')->find_all();
-		$this->response->body('<p>'.$tasks[0]->name.'</p>');
+		$projects = ORM::factory('projects')->find_all();
+		$this->response->body('<table cellpadding="2" cellspacing="0" border="1"><tr><th>Project ID</th><th>Project Task</th></tr><tr><td>'.$projects[0]->id.'</td><td>'.$tasks[0]->name.'</td></tr></table>');
 		//var_dump($tasks);
 	}
 	
